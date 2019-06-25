5f26e8cd4763f4fc27799836944c1da3dea01b09
wangzhibin <wangzhibin@anbai.com>
2019-06-25 10:02:04 +0800

    调整test

--- src/main/java/com/collection/HashMapTest.java
+++ src/main/java/com/collection/HashMapTest.java
@@ -17,6 +17,41 @@ public class HashMapTest {
     }
 
     public static void main(String[] args) {
+        testMap();
+
+//        System.out.println(roundUpToPowerOf2(6));
+//        System.out.println(roundUpToPowerOf2(12));
+//        System.out.println(roundUpToPowerOf2(18));
+//        System.out.println(roundUpToPowerOf2(25));
+//
+        // System.out.println(highestOneBit(5));
+        // :  00000000 00000000 00000000 00000100 4的原码
+        // :  10000000 00000000 00000000 00000100 -4的原码 符号位取反，其他不变
+        // :  11111111 11111111 11111111 11111011 -4的反码 符号位不变，其他取反
+        // :  11111111 11111111 11111111 11111100 -4的补码 计算机存储 反码+1
+
+    }
+
+    private static int roundUpToPowerOf2(int number) {
+        int maxCapacity = 1 << 30;
+
+        // assert number >= 0 : "number must be non-negative";
+        return number >= maxCapacity
+                ? maxCapacity
+                : (number > 1) ? highestOneBit((number - 1) << 1) : 1;
+    }
+
+    private static int highestOneBit (int i) {
+        i |= (i >>  1);
+        i |= (i >>  2);
+        i |= (i >>  4);
+        i |= (i >>  8);
+        i |= (i >> 16);
+        return i - (i >>> 1);
+    }
+
+
+    private static void testMap() {
         HashMapTest h1 = new HashMapTest(1);
         HashMapTest h2 = new HashMapTest(1);
 
@@ -27,17 +62,16 @@ public class HashMapTest {
         map.put(h1, 1);
         map.put(h2, 2);
 
-        Integer v1 = map.get(h1);
-        System.out.println("v1\t : " + v1);
-        Integer v2 = map.get(h2);
-        System.out.println("v2\t : " + v2);
 
-        String abc = "a";
+        h1 = null;
+        h2 = null;
 
-        Thread thread;
+        System.out.println(map);
 
-        Object o = new Object();
-
+//        Integer v1 = map.get(h1);
+//        System.out.println("v1\t : " + v1);
+//        Integer v2 = map.get(h2);
+//        System.out.println("v2\t : " + v2);
     }
 
     @Override
---
+++ src/main/java/com/collection/HashMapTest2.java
@@ -0,0 +1,76 @@
+package com.collection;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * 说明：HashMapTest2
+ *
+ * @author ShujuboDev
+ */
+public class HashMapTest2 {
+    public static void main(String[] args) {
+        testMap();
+    }
+
+    private static void testMap() {
+        Set<Student> set = new HashSet<>();
+        Student s1 = new Student("zs", 15);
+        Student s2 = new Student("zs", 15);
+
+        set.add(s1);
+        set.add(s2);
+
+        System.out.println(set);
+
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
