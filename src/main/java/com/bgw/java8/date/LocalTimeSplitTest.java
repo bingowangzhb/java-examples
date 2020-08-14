2638e9f052ca372aba65346dfc72c72482d92777
wangzhibin <zhibin.wang@ambow.com>
2020-08-14 18:46:38 +0800

    update

---
+++ src/main/java/com/bgw/java8/date/LocalTimeSplitTest.java
@@ -0,0 +1,44 @@
+package com.bgw.java8.date;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * LocalTimeSplitTest
+ *
+ * @author zhibin.wang
+ * @since 2020/08/14 16:00
+ */
+public class LocalTimeSplitTest {
+
+    public static void main(String[] args) {
+
+        LocalTime l1 = LocalTime.of(8, 30, 0);
+        LocalTime l2 = LocalTime.of(12, 0, 0);
+
+        long size = ChronoUnit.MINUTES.between(l1, l2) / 30 + 1;
+        System.out.println(size);
+        List<LocalTime> lts = Stream.iterate(l1, h -> h.plusMinutes(30))
+                .limit(size)
+                .collect(Collectors.toList());
+
+        System.out.println(lts);
+
+
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+
+
+
+
+
+
+
+
+    }
+}
